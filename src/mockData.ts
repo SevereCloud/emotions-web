a730abdd2260f126ef75434cc1638aaa2a2a3b66
VChertukhin <vchertukhin@protonmail.com>
2020-09-19 11:26:10 +0300

    void mock data

--- src/mockData.ts
+++ src/mockData.ts
@@ -2,86 +2,92 @@ import type { Wall } from './api';
 import type { ThemeWalls } from './types';
 
 const geo = {
-  type: '',
-  coordinates: '1.1 2.3',
-  place: {
-    created: 0,
-    id: 0,
-    is_deleted: false,
-    latitude: 30.3,
-    longitude: 31.4,
-    title: '',
-    total_checkins: 0,
-    updated: 0,
-    country: '',
-    city: '',
-  },
+    type: '',
+    coordinates: '1.1 2.3',
+    place: {
+        created: 0,
+        id: 0,
+        is_deleted: false,
+        latitude: 30.3,
+        longitude: 31.4,
+        title: '',
+        total_checkins: 0,
+        updated: 0,
+        country: '',
+        city: '',
+    },
 };
 
 const autumn: Wall[] = [
-  {
-    id: 1,
-    date: 1,
-    owner_id: 1,
-    from_id: 1,
-    post_type: '',
-    text: 'Бонжур',
-    attachments: [],
-    comments: {
-      count: 121,
+    {
+        id: 1,
+        date: 1,
+        owner_id: 1,
+        from_id: 1,
+        post_type: '',
+        text: 'Бонжур',
+        attachments: [],
+        comments: {
+            count: 121,
+        },
+        likes: {
+            count: 455,
+        },
+        reposts: {
+            count: 293,
+            wall_count: 123,
+            mail_count: 11,
+        },
+        views: {
+            count: 12332,
+        },
+        is_favorite: false,
+        geo,
     },
-    likes: {
-      count: 455,
+    {
+        id: -1,
+        date: 1,
+        owner_id: 1,
+        from_id: 1,
+        post_type: '',
+        text: 'Бонжур тужур',
+        attachments: [],
+        comments: {
+            count: 121,
+        },
+        likes: {
+            count: 455,
+        },
+        reposts: {
+            count: 293,
+            wall_count: 123,
+            mail_count: 11,
+        },
+        views: {
+            count: 12332,
+        },
+        is_favorite: false,
+        geo,
     },
-    reposts: {
-      count: 293,
-      wall_count: 123,
-      mail_count: 11,
-    },
-    views: {
-      count: 12332,
-    },
-    is_favorite: false,
-    geo,
-  },
-  {
-    id: -1,
-    date: 1,
-    owner_id: 1,
-    from_id: 1,
-    post_type: '',
-    text: 'Бонжур тужур',
-    attachments: [],
-    comments: {
-      count: 121,
-    },
-    likes: {
-      count: 455,
-    },
-    reposts: {
-      count: 293,
-      wall_count: 123,
-      mail_count: 11,
-    },
-    views: {
-      count: 12332,
-    },
-    is_favorite: false,
-    geo,
-  },
 ];
 
+export const mockThemeWallsVoid: ThemeWalls = {
+    fall: [],
+    work: [],
+    quarantine: [],
+    film: [],
+    art: [],
+    game: [],
+    comedy: [],
+    photo: [],
+};
+
 export const mockThemeWalls: ThemeWalls = {
-  fall: [...autumn, ...autumn],
-  work: [],
-  quarantine: [],
-  film: [],
-  art: [],
-  game: [],
-  comedy: [],
-  photo: [],
+    ...mockThemeWallsVoid,
+    fall: [...autumn, ...autumn],
 };
 
 export default {
-  themeWalls: mockThemeWalls,
+    themeWallsVoid: mockThemeWallsVoid,
+    themeWalls: mockThemeWalls,
 };
