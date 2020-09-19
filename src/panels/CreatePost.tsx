613505f5bdc008185c7d4128305d0bd9e61e26e6
VChertukhin <vchertukhin@protonmail.com>
2020-09-19 14:09:07 +0300

    post creation textarea

--- src/panels/CreatePost.tsx
+++ src/panels/CreatePost.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { ChangeEvent, useContext, useState } from 'react';
 import {
   PanelHeader,
   PanelHeaderClose,
@@ -10,6 +10,10 @@ import AppCTX from '../appContext';
 
 export const CreatePost = () => {
   const { setPanel } = useContext(AppCTX) as ctxValue;
+  const [postText, setPostText] = useState('');
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setPostText(e.target.value)
+  }
   return (
     <>
       <PanelHeader
@@ -22,6 +26,26 @@ export const CreatePost = () => {
           style={{ borderRadius: '50%' }}
         />
       </PanelHeader>
+      <textarea
+        autoFocus
+        onChange={handleChange}
+        value={postText}
+        placeholder="Что у вас нового?"
+        style={{
+          padding: '16px',
+          width: '100%',
+          height: 'calc(100vh - 100px)',
+          outline: 'none',
+          border: 'none',
+          overflow: 'auto',
+          WebkitBoxShadow: 'none',
+          MozBoxShadow: 'none',
+          resize: 'none',
+          caretColor: '#4986cc',
+          fontSize: '1.25em',
+          backgroundColor: 'var(--background_content)'
+        }}
+      />
     </>
   )
 }
